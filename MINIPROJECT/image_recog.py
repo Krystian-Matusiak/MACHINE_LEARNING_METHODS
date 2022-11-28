a2151b3ab78564b58ee4fea8af3e8705044163d3
Krystian <krystian.matusiak13@gmail.com>
2022-11-28 19:48:13 +0100

    Selected animals, initial NN applied

--- MINIPROJECT/image_recog.py
+++ MINIPROJECT/image_recog.py
@@ -5,6 +5,13 @@ try:
     import pandas as pd
     import matplotlib.pyplot as plt
     from sklearn.decomposition import PCA
+    from tensorflow import keras
+    from tensorflow.keras import layers, models
+    from tensorflow.keras.preprocessing import image
+    from tensorflow.keras.preprocessing.image import ImageDataGenerator
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import Conv2D, Flatten, MaxPooling2D, Dense, Dropout, GlobalAveragePooling2D
+    from tensorflow.keras import optimizers, losses
     import tensorflow as tf
     from pathlib import Path
     import os
@@ -35,6 +42,7 @@ def import_dataset():
 if __name__ == "__main__":
     image_df = import_dataset()
     print(image_df)
+    data = os.getcwd() + "/MINIPROJECT/SEA_ANIMALS"
 
     random_index = np.random.randint(0, len(image_df), 16)
     fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(10, 10),
@@ -46,3 +54,47 @@ if __name__ == "__main__":
         ax.set_title(image_df.Label[random_index[i]])
     plt.tight_layout()
     plt.show()
+
+    train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2,
+                                       shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode='nearest')
+
+    train_images = train_datagen.flow_from_directory(
+        data,
+        target_size=(224, 224),
+        batch_size=34,
+        class_mode='categorical',
+        subset='training')  # set as training data
+
+    mobile_model = Sequential()
+
+    pretrained_model = tf.keras.applications.MobileNetV2(
+        input_shape=(224, 224, 3),
+        include_top=False,
+        weights='imagenet',
+        pooling='avg'
+    )
+
+    pretrained_model.trainable = False
+
+    mobile_model.add(pretrained_model)
+    mobile_model.add(Flatten())
+    mobile_model.add(Dense(212, activation='relu'))
+    mobile_model.add(Dropout(0.2))
+    mobile_model.add(Dense(7, activation='softmax'))
+
+    mobile_model.compile(loss='categorical_crossentropy',
+                         optimizer=tf.keras.optimizers.Adam(), metrics=['accuracy'])
+
+    history = mobile_model.fit(train_images,
+                               steps_per_epoch=len(train_images),
+                               epochs=1)
+
+    print(history.history.keys())
+    plt.plot(history.history['accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train'], loc='upper left')
+    plt.show()
+    print(2)
+    print(2)
